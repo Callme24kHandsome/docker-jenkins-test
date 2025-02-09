7d9aa882e594f803fa7998ec7a7b5427741ab873
callme24kHandsome <815665948@qq.com>
2025-02-09 14:33:41 +0800

    first commit

---
+++ src/main/java/org/example/TestController.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * @Description:
+ * @Create: Date:2025年01月23日
+ */
+@RestController("/test")
+public class TestController {
+
+    @GetMapping("/aaa")
+    public Object aaa(){
+        return "bbb";
+    }
+}
